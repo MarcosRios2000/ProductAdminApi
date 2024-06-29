d04746132b8d497461a4fd20b839ccc4bd72e725
MarcosRios2000 <marcosrioos@gmail.com>
2024-06-29 10:56:49 -0300

    feat: product model created with decorators

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -10,7 +10,8 @@ const db = new Sequelize(process.env.DATABASE_URL, {
         ssl: {
             require: false
         }
-    }
+    },
+    models: [__dirname + '/../models/**/*.ts']
 })
 
 export default db
