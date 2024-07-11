4d509c276cfc238ac935ccfebaba070723616e0f
MarcosRios2000 <marcosrioos@gmail.com>
2024-07-11 20:56:32 -0300

    feat: route get api removed && add swagger template

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,21 +1,7 @@
-import request from 'supertest'
-import server, { connectDB } from '../server'
+import { connectDB } from '../server'
 import db from '../config/db'
 
 
-
-describe('GET /api', () => {
-    it('should send back a json response', async () => {
-        const res = await request(server).get('/api')
-
-        expect(res.status).toBe(200)
-        expect(res.headers['content-type']).toMatch(/json/)
-
-
-        expect(res.status).not.toBe(404)
-
-    })
-})
 
 
 jest.mock('../config/db')
