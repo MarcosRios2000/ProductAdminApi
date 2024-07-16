a14f0baeccafd3159b7d98211bf31f0feb4797f2
MarcosRios2000 <marcosrioos@gmail.com>
2024-07-16 20:46:05 -0300

    feat: add cors && fix: cors issue solved

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import colors from 'colors'
+import cors, { CorsOptions } from 'cors'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec from './config/swagger'
 import router from './router'
@@ -21,6 +22,16 @@ connectDB()
 // Instancia de express
 const server = express()
 
+const corsOptions : CorsOptions= {
+    origin: function(origin, cb) {
+        if(origin === process.env.FRONTEND_URL){
+            cb(null, true)
+        } else {
+            cb(new Error('Error de CORS'))
+        }
+    }
+}
+server.use(cors(corsOptions))
 
 // Leer datos de formularios
 server.use(express.json())
