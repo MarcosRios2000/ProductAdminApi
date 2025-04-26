b24ce629ef26238c205d46ec3ceb754fb101934e
MarcosRios2000 <marcosrioos@gmail.com>
2025-04-25 22:15:18 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,46 @@
-import express from 'express'
-import colors from 'colors'
-import cors, { CorsOptions } from 'cors'
-import morgan from 'morgan'
-import swaggerUi from 'swagger-ui-express'
-import swaggerSpec from './config/swagger'
-import router from './router'
-import db from './config/db'
+import express from 'express';
+import colors from 'colors';
+import cors, { CorsOptions } from 'cors';
+import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './config/swagger';
+import router from './router';
+import db from './config/db';
 
-
-//conectar la db
-export async function connectDB(){
+// Conectar la db
+export async function connectDB() {
     try {
-        await db.authenticate()
-        db.sync()
-        // console.log(colors.bgGreen.bold('Conexión exitosa a la db'))
+        await db.authenticate();
+        db.sync();
+        // console.log(colors.bgGreen.bold('Conexión exitosa a la db'));
     } catch (error) {
-        console.log(colors.bgRed.bold('Error al conectarse a la db'))
+        console.log(colors.bgRed.bold('Error al conectarse a la db'));
     }
 }
-connectDB()
+connectDB();
 
-// Instancia de express
-const server = express()
 
-const corsOptions : CorsOptions= {
+const server = express();
+
+
+server.use('/docs', cors(), swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+const corsOptions: CorsOptions = {
     origin: function(origin, cb) {
-        if(origin === process.env.FRONTEND_URL){
-            cb(null, true)
+        if (origin === process.env.FRONTEND_URL || !origin) {
+            cb(null, true);
         } else {
-            cb(new Error('Error de CORS'))
+            cb(new Error('Error de CORS'));
         }
     }
-}
-server.use(cors(corsOptions))
-// Leer datos de formularios
-server.use(express.json())
-
-server.use(morgan('dev'))
-server.use('/api/products', router)
+};
+server.use(cors(corsOptions));
 
 
-server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+server.use(express.json());
+server.use(morgan('dev'));
 
-export default server
+server.use('/api/products', router);
+
+export default server;
