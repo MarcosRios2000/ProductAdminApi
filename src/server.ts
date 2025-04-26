9f37793d62bcb72d869aef4f797252e3c3357ddb
MarcosRios2000 <marcosrioos@gmail.com>
2025-04-25 21:59:14 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -23,12 +23,16 @@ connectDB()
 // Instancia de express
 const server = express()
 
-const corsOptions: CorsOptions = {
-    origin: [process.env.FRONTEND_URL || ''], 
-    credentials: true
-};
+const corsOptions : CorsOptions= {
+    origin: function(origin, cb) {
+        if(origin === process.env.FRONTEND_URL){
+            cb(null, true)
+        } else {
+            cb(new Error('Error de CORS'))
+        }
+    }
+}
 server.use(cors(corsOptions))
-
 // Leer datos de formularios
 server.use(express.json())
 
